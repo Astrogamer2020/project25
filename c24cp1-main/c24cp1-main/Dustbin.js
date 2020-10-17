72c4be0bf5605e713c5212564fd78c6ba0919c72
Astrogamer2020 <70177647+Astrogamer2020@users.noreply.github.com>
2020-10-17 10:33:02 -0400

    Add files via upload

---
+++ c24cp1-main/c24cp1-main/Dustbin.js
@@ -0,0 +1,35 @@
+class Dustbin {
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic: true,
+            restitution: 0.8,
+            density: 1.0,
+            friction: 1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("dustbingreen.png");
+        World.add(world, this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        push();
+        fill("red");
+        rectMode(CENTER);
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+    }
+    addPic(){
+        var pos = this.body.position;
+        push();
+        fill("red");
+        image(this.image,this.x,this.y,this.width,this.height);
+        rectMode(CENTER);
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+     
+
+    }
+}
---
+++ c24cp1-main/c24cp1-main/README.md
@@ -0,0 +1,2 @@
+### Crumpled Paper 1
+# https://iambushra37.github.io/CrumpledPaper-1/
